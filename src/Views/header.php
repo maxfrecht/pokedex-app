4516e6c81e189723753daad04f78ef5848bfb7b9
MaxFrecht <max.frecht@gmail.com>
2021-06-24 22:55:30 +0200

    add pokemon icon and title

--- src/Views/header.php
+++ src/Views/header.php
@@ -6,7 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../Styles/Css/style.css">
-    <title>Document</title>
+    <link rel="icon" href="../assets/images/pokeball.svg">
+    <title>Pokedex</title>
 </head>
 <body>
 
